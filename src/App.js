1033c86e51d873d921b65b61b9f49740078a088c
canknee <kennethslin@gmail.com>
2024-02-22 14:36:04 -0500

    feat: header for all pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'antd'
+import Header from './components/header';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          TESTING KENNY WAS HERE ZZZZZZZZ
-          TESTING CAMBAILEY WAS HERE ZZZZZZZZ
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+            TESTING KENNY WAS HERE ZZZZZZZZ
+            TESTING CAMBAILEY WAS HERE ZZZZZZZZ
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
   );
 }
 
